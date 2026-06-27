cb2c1dc817c9bb20ba2db78883e2a4dec570e4b2
Nikita Khomitsevych <hamsternik9@gmail.com>
2026-06-27 14:12:24 +0100

    define supported and not supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "PhantomKeychain",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .watchOS(.v9),
+        
+        // tvOS omitted: SecItem compiles, but tvOS has not persistent on-device Keychain.
+        // Items are not guaranteed to survive termination/reboot, and `...ThisDeviceOnly`
+        // access opts out in the iCloud fallback. Back to this later on!
+        // .tvOS(.v16),
+        
+        // visionOS ommited: Keychain works b/c the OS is iOS-based, but untested and
+        // out of CI/CD pipeline. Re-enable once there is a verified build/test pipeline.
+        // btw visionOS does require swift-tools-version >= 5.9. Back to this later on!
+        // .visionOS(.v1),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
